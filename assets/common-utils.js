3a068fb4c6d2d8cc37d74877003ca626831a8165
maforgit <maforgit@gmail.com>
2016-09-13 15:58:00 +0300

    a

--- assets/common-utils.js
+++ assets/common-utils.js
@@ -254,13 +254,13 @@ function replaceAll(str, find, replace) {
 }
 
 function reverse(s){
-    return s.split("").reverse().join("");
+	return s.split("").reverse().join("");
 }
 
 function strcmp(a, b) {
-    if (a < b) return -1;
-    if (a > b) return 1;
-    return 0;
+	if (a < b) return -1;
+	if (a > b) return 1;
+	return 0;
 }
 
 function sortObj(obj) {
@@ -353,9 +353,9 @@ function saveTextAsFile(textToSave, fileNameToSaveAs) {
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // polyfil for IE
 if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function(searchString, position){
-      position = position || 0;
-      return this.substr(position, searchString.length) === searchString;
+	String.prototype.startsWith = function(searchString, position){
+	  position = position || 0;
+	  return this.substr(position, searchString.length) === searchString;
   };
 }
 
@@ -387,77 +387,77 @@ https://gist.github.com/zziuni/3741933
 */
 
 function getIPs(stunUrl, callback){
-    var ip_dups = {};
+	var ip_dups = {};
 
-    //compatibility for firefox and chrome
-    var RTCPeerConnection = window.RTCPeerConnection
-        || window.mozRTCPeerConnection
-        || window.webkitRTCPeerConnection;
-    var useWebKit = !!window.webkitRTCPeerConnection;
+	//compatibility for firefox and chrome
+	var RTCPeerConnection = window.RTCPeerConnection
+		|| window.mozRTCPeerConnection
+		|| window.webkitRTCPeerConnection;
+	var useWebKit = !!window.webkitRTCPeerConnection;
 
-    //bypass naive webrtc blocking using an iframe
-    if(!RTCPeerConnection){
-        //NOTE: you need to have an iframe in the page right above the script tag
-        //
-        //<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>
-        //<script>...getIPs called in here...
-        //
-        var win = iframe.contentWindow;
-        RTCPeerConnection = win.RTCPeerConnection
-            || win.mozRTCPeerConnection
-            || win.webkitRTCPeerConnection;
-        useWebKit = !!win.webkitRTCPeerConnection;
-    }
+	//bypass naive webrtc blocking using an iframe
+	if(!RTCPeerConnection){
+		//NOTE: you need to have an iframe in the page right above the script tag
+		//
+		//<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>
+		//<script>...getIPs called in here...
+		//
+		var win = iframe.contentWindow;
+		RTCPeerConnection = win.RTCPeerConnection
+			|| win.mozRTCPeerConnection
+			|| win.webkitRTCPeerConnection;
+		useWebKit = !!win.webkitRTCPeerConnection;
+	}
 
-    //minimal requirements for data connection
-    var mediaConstraints = {
-        optional: [{RtpDataChannels: true}]
-    };
-    // var servers = {iceServers: [{urls: "stun:stun.ekiga.net"}]};
-    var servers = {iceServers: [{urls: stunUrl}]};
+	//minimal requirements for data connection
+	var mediaConstraints = {
+		optional: [{RtpDataChannels: true}]
+	};
+	// var servers = {iceServers: [{urls: "stun:stun.ekiga.net"}]};
+	var servers = {iceServers: [{urls: stunUrl}]};
 
-    //construct a new RTCPeerConnection
-    var pc = new RTCPeerConnection(servers, mediaConstraints);
+	//construct a new RTCPeerConnection
+	var pc = new RTCPeerConnection(servers, mediaConstraints);
 
-    function handleCandidate(candidate){
-        //match just the IP address
-        var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
-        var ip_addr = ip_regex.exec(candidate)[1];
+	function handleCandidate(candidate){
+		//match just the IP address
+		var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
+		var ip_addr = ip_regex.exec(candidate)[1];
 
-        //remove duplicates
-        if(ip_dups[ip_addr] === undefined)
-            callback(ip_addr);
+		//remove duplicates
+		if(ip_dups[ip_addr] === undefined)
+			callback(ip_addr);
 
-        ip_dups[ip_addr] = true;
-    }
+		ip_dups[ip_addr] = true;
+	}
 
-    //listen for candidate events
-    pc.onicecandidate = function(ice){
+	//listen for candidate events
+	pc.onicecandidate = function(ice){
 
-        //skip non-candidate events
-        if(ice.candidate)
-            handleCandidate(ice.candidate.candidate);
-    };
+		//skip non-candidate events
+		if(ice.candidate)
+			handleCandidate(ice.candidate.candidate);
+	};
 
-    //create a bogus data channel
-    pc.createDataChannel("");
+	//create a bogus data channel
+	pc.createDataChannel("");
 
-    //create an offer sdp
-    pc.createOffer(function(result){
+	//create an offer sdp
+	pc.createOffer(function(result){
 
-        //trigger the stun server request
-        pc.setLocalDescription(result, function(){}, function(){});
+		//trigger the stun server request
+		pc.setLocalDescription(result, function(){}, function(){});
 
-    }, function(){});
+	}, function(){});
 
-    //wait for a while to let everything done
-    setTimeout(function(){
-        //read candidate info from local description
-        var lines = pc.localDescription.sdp.split('\n');
+	//wait for a while to let everything done
+	setTimeout(function(){
+		//read candidate info from local description
+		var lines = pc.localDescription.sdp.split('\n');
 
-        lines.forEach(function(line){
-            if(line.indexOf('a=candidate:') === 0)
-                handleCandidate(line);
-        });
-    }, 1000);
+		lines.forEach(function(line){
+			if(line.indexOf('a=candidate:') === 0)
+				handleCandidate(line);
+		});
+	}, 1000);
 }
--- assets/resource-utils.js
+++ assets/resource-utils.js
@@ -248,10 +248,10 @@ function getImage(str) {
 			key = str.substring(1, pos1);
 			index = parseInt(str.substring(pos1 + 1, pos2));
 		}
-        //console.log(key);
+		//console.log(key);
 		return _map[key][index];
 	}
-    return str;
+	return str;
 }
 
 function getBackground(str) {
